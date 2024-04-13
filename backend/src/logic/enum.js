afa3d8806108b6425d29f661086bc4e98d329dd0
fforfaii <156507299+fforfaii@users.noreply.github.com>
2024-04-13 23:32:22 +0700

    implement 5 skills

--- backend/src/logic/enum.js
+++ backend/src/logic/enum.js
@@ -1,3 +1,5 @@
+import { checkWinner } from "./utilities";
+
 export const gameStatesEnum = [
   "waiting_player",
   "break",
@@ -11,11 +13,11 @@ export const resultsEnum = ['player1', 'player2', 'draw'];
 export const cardsEnum = {
 
   Paper_Loss: {
-    description: "สามารถใช้เพื่อบังคับเปลี่ยนเป้าที่ฝั่งตรงข้ามออกเป็น กระดาษ",
+    description: "สามารถใช้เพื่อบังคับเปลี่ยนเป้าที่ฝั่งตรงข้ามออกเป็น'กระดาษ'",
     preSkill: function (game, player) {
-      const number = player.getNumber == 1 ? 2 : 1;
-      game.getPlayerByNumber(number);
-      player.setAvailableChoices(
+      const number = player.getNumber() == 1 ? 2 : 1;
+      opponent = game.getPlayerByNumber(number);
+      opponent.setAvailableChoices(
         choicesEnum.filter((choice) => choice != "paper")
       );
     },
@@ -25,11 +27,11 @@ export const cardsEnum = {
   },
 
   Rock_Out: {
-    description: "สามารถใช้เพื่อบังคับเปลี่ยนเป้าที่ฝั่งตรงข้ามออกเป็น ค้อน",
+    description: "สามารถใช้เพื่อบังคับเปลี่ยนเป้าที่ฝั่งตรงข้ามออกเป็น'ค้อน'",
     preSkill: function (game,player) {
-      const number = player.getNumber == 1 ? 2 : 1;
-      game.getPlayerByNumber(number);
-      player.setAvailableChoices(
+      const number = player.getNumber() == 1 ? 2 : 1;
+      opponent = game.getPlayerByNumber(number);
+      opponent.setAvailableChoices(
         choicesEnum.filter((choice) => choice != "rock")
       );
     },
@@ -39,16 +41,105 @@ export const cardsEnum = {
   },
 
   Hopecutting_Scissors: {
-    description: "สามารถใช้เพื่อบังคับเปลี่ยนเป้าที่ฝั่งตรงข้ามออกเป็น กรรไกร",
+    description: "สามารถใช้เพื่อบังคับเปลี่ยนเป้าที่ฝั่งตรงข้ามออกเป็น'กรรไกร'",
     preSkill: function (game,player) {
-      const number = player.getNumber == 1 ? 2 : 1;
-      game.getPlayerByNumber(number);
-      player.setAvailableChoices(
+      const number = player.getNumber() == 1 ? 2 : 1;
+      opponent = game.getPlayerByNumber(number);
+      opponent.setAvailableChoices(
         choicesEnum.filter((choice) => choice != "scissors")
       );
     },
     postSkill: function (game, player) {
       checkWinner(game);
+    }
+  },
+
+  OneMoreTime: {
+    description: "สามารถใช้เพื่อให้ฝั่งตรงข้ามไม่ได้แต้ม หากเขาเป่าชนะในตานี้",
+    preSkill: function (game,player) {
+      
+    },
+    postSkill: function (game,player) {
+      const number = player.getNumber() == 1 ? 2 : 1;
+      opponent = game.getPlayerByNumber(number)
+      oppo_ch = opponent.getChoice();
+      player_ch = player.getChoice();
+      if (
+        (oppo_ch === "rock" && player_ch === "scissors") ||
+        (oppo_ch === "paper" && player_ch === "rock") ||
+        (oppo_ch === "scissors" && player_ch === "paper")
+      ) {
+        // set ให้ฝั่งตรงข้ามออกช้อยเดียวกันเพื่อให้ฝั่งตรงข้ามไม่ได้แต้มตอนคิดคะแนน
+        opponent.setChoice(player_ch);
+      }
+      checkWinner(game);
+    }
+  },
+
+  Makeit_Or_Breakit: {
+    description: "ผู้ชนะในตานี้จะได้รับ 2 แต้ม",
+    preSkill: function (game,player) {
+      
+    },
+    postSkill: function (game,player) {
+      const number = player.getNumber() == 1 ? 2 : 1;
+      opponent = game.getPlayerByNumber(number)
+      prescore_oppo = opponent.getScore();
+      prescore_player = player.getScore();
+      checkWinner(game);
+      if (player.getScore() > prescore_player) {
+        player.setScore(player.getScore() + 1);
+      } else (opponent.getScore() > prescore_oppo) {
+        opponent.setScore(opponent.getScore() + 1);
+      }
+    }
+  },
+
+  Rock_You: {
+    description: "สามารถใช้เพื่อให้ฝั่งตรงข้ามไม่ได้คะแนน หากเขาเป่าชนะด้วย'ค้อน'",
+    preSkill: function (game,player) {
+
+    },
+    postSkill: function (game,player) {
+      const number = player.getNumber() == 1 ? 2 : 1;
+      opponent = game.getPlayerByNumber(number)
+      prescore_oppo = opponent.getScore();
+      checkWinner(game);
+      if (opponent.getScore() > prescore_oppo && opponent.getChoice() == "rock") {
+        opponent.setScore(prescore_oppo);
+      }
+    }
+  },
+
+  Paper_Cut: {
+    description: "สามารถใช้เพื่อให้ฝั่งตรงข้ามไม่ได้คะแนน หากเขาเป่าชนะด้วย'กระดาษ'",
+    preSkill: function(game,player) {
+
+    },
+    postSkill: function(game,player) {
+      const number = player.getNumber() == 1 ? 2 : 1;
+      opponent = game.getPlayerByNumber(number)
+      prescore_oppo = opponent.getScore();
+      checkWinner(game);
+      if (opponent.getScore() > prescore_oppo && opponent.getChoice() == "paper") {
+        opponent.setScore(prescore_oppo);
+      }
+    }
+  },
+
+  Snipping_Victory: {
+    description: "สามารถใช้เพื่อให้ฝั่งตรงข้ามไม่ได้คะแนน หากเขาเป่าชนะด้วย'กรรไกร'",
+    preSkill: function(game,player) {
+
+    },
+    postSkill: function(game,player) {
+      const number = player.getNumber() == 1 ? 2 : 1;
+      opponent = game.getPlayerByNumber(number)
+      prescore_oppo = opponent.getScore();
+      checkWinner(game);
+      if (opponent.getScore() > prescore_oppo && opponent.getChoice() == "scissors") {
+        opponent.setScore(prescore_oppo);
+      }
     }
   },
 }
